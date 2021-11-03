426a891c1956beeff73f166300dcae100216e7ad
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-11-03 16:03:07 +0000

    Add project files.

---
+++ ATM/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sets the name of the console
+            Console.Title = "Trojan Bank Terminal";
+
+            // sets the colour of the text
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Please enter your 4 digit pin: ");
+
+            //stores the input as a string variable
+
+            string pin = RequestPIN();
+            Console.WriteLine();
+            Console.WriteLine("Your pin is " + pin);
+            Console.ReadKey();
+        }
+
+        private static string RequestPIN()
+        {
+            StringBuilder sb = new StringBuilder();
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+
+                if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    sb.Append(keyInfo.KeyChar);
+                    Console.Write("*");
+                }
+            } while (keyInfo.Key != ConsoleKey.Enter);
+            {
+                return sb.ToString();
+            }
+
+        }
+
+    }
+}
