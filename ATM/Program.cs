d76fd5469648ca642ad61344e6854b9d2cd42850
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2021-11-04 09:37:32 +0000

    building menu

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -18,10 +18,11 @@ namespace ATM
             //stores the input as a string variable
 
             string pin = RequestPIN();
-            Console.WriteLine();
+            Console.WriteLine("");
+            Console.WriteLine("");
             Console.WriteLine("Welcome to Trojan Bank, press enter to continue");
 
-
+            MainMenu();
 
             Console.ReadKey();
         }
@@ -49,20 +50,70 @@ namespace ATM
 
         static void MainMenu()
         {
+            // sets the name of the console
+            Console.Title = "Trojan Bank Terminal";
 
+            // sets the colour of the text
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("");
+            Console.WriteLine("Options Menu");
+            Console.WriteLine("");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Balance");
+            Console.WriteLine("4. Exit");
+
+            string choice;
+            choice = Console.ReadLine();
         }
 
         static void Deposit()
         {
+            // sets the name of the console
+            Console.Title = "Trojan Bank Terminal";
+
+            // sets the colour of the text
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("");
+            Console.WriteLine("Deposits");
+            Console.WriteLine("");
 
         }
 
         static void Balance()
         {
+            // sets the name of the console
+            Console.Title = "Trojan Bank Terminal";
+
+            // sets the colour of the text
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("");
+            Console.WriteLine("Current Balance");
+            Console.WriteLine("");
 
         }
 
         static void Withdraw()
+        {
+            // sets the name of the console
+            Console.Title = "Trojan Bank Terminal";
+
+            // sets the colour of the text
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine("");
+            Console.WriteLine("Withdrawals");
+            Console.WriteLine("");
+        }
+
+        static void Exit()
         {
 
         }
